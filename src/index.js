15b246966e85ea2ce598f08f634cd1d4b2f49c2d
Juanes-xd <juanestebanortizbejarano@gmail.com>
2023-01-27 20:50:19 -0500

    commit 1

--- src/index.js
+++ src/index.js
@@ -1,63 +1,65 @@
 const { ApolloServer, gql } = require("apollo-server");
+const axios = require("axios");
 
+async function datos() {
+  try {
+    const response = await axios.get(
+      "https://api.breakingbadquotes.xyz/v1/quotes/10"
+    );
+    console.log(response.data);
+    //breakingbadquotes.push(response.data);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+let breakingbadquotes = datos();
 
 const typeDefs = gql`
   type Book {
-    id: String  
+    id: String
     title: String
     author: String
   }
 
   type Query {
-    Getbooks: [Book],
-    Getbook(id:String!):[Book]
+    Getbreakingbadquotes: [Book]
+    Getbook(id: String!): [Book]
   }
   type Mutation {
-      CreateBook(id: String!,title: String!, author: String!): Book
-      DeleteBook(id: String!): Book
-      UpdateBook(id: String!,title: String!, author: String!): Book 
+    CreateBook(id: String!, title: String!, author: String!): Book
+    DeleteBook(id: String!): Book
+    UpdateBook(id: String!, title: String!, author: String!): Book
   }
 `;
 
-let books = [
-    {
-      id:"1",
-      title: 'The Awakening',
-      author: 'Kate Chopin',
+const resolvers = {
+  Mutation: {
+    CreateBook: (_, arg) => {
+      breakingbadquotes.push(arg);
+      return arg;
     },
-    {
-      id:"2",  
-      title: 'City of Glass',
-      author: 'Paul Auster',
+    DeleteBook: (_, arg) => {
+      let finalbreakingbadquotes = breakingbadquotes.filter(
+        (book) => book.id != arg.id
+      );
+      let bookdeleted = breakingbadquotes.find((book) => book.id == arg.id);
+      breakingbadquotes = [...finalbreakingbadquotes];
+      return bookdeleted;
     },
-    {
-       id:"3",  
-       title: 'Del amor y otros demonios',
-       author: 'Gabriel garcia Marquez',
-    }
-  ];
-  const resolvers = {
-    Mutation: {
-        CreateBook: (_,arg) => {books.push(arg); return arg},
-        DeleteBook: (_,arg) => { 
-                                 let finalbooks=books.filter(book => book.id != arg.id);
-                                 let bookdeleted = books.find(book => book.id == arg.id );   
-                                 books = [...finalbooks]; 
-                                 return bookdeleted
-                                },
-        UpdateBook:(_,arg) => {  let objIdx = books.findIndex(book => book.id == arg.id);
-                                 books[objIdx] = arg
-                                 return arg   
-             
-                              }                        
-
-    },  
-    Query: {
-      Getbooks: () => books,
-      Getbook: (_,arg) => books.find(number => number.id==arg.id)
+    UpdateBook: (_, arg) => {
+      let objIdx = breakingbadquotes.findIndex((book) => book.id == arg.id);
+      breakingbadquotes[objIdx] = arg;
+      return arg;
     },
-  };
-
+  },
+  Query: {
+    Getbreakingbadquotes: () => breakingbadquotes,
+    Getbook: (_, arg) =>
+      breakingbadquotes.find((number) => number.id == arg.id),
+  },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
